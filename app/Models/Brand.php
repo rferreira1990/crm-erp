5217b41dc50670acc2fd4e668252f0275f0ba759
Ricardo <rfpav.ric@gmail.com>
2026-03-19 18:56:48 +0000

    19 de mar. de 2026, 18:56

---
+++ app/Models/Brand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
+}
