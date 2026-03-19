a3eaf1133adbf4488581899727fff0c1582ef2c5
Ricardo <rfpav.ric@gmail.com>
2026-03-19 18:58:05 +0000

    19 de mar. de 2026, 18:58

--- database/seeders/UnitSeeder.php
+++ database/seeders/UnitSeeder.php
@@ -2,16 +2,34 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Unit;
 use Illuminate\Database\Seeder;
 
 class UnitSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $rows = [
+            ['code' => 'UN', 'name' => 'Unidade', 'factor' => 1],
+            ['code' => 'M', 'name' => 'Metro', 'factor' => 1],
+            ['code' => 'M2', 'name' => 'Metro quadrado', 'factor' => 1],
+            ['code' => 'M3', 'name' => 'Metro cúbico', 'factor' => 1],
+            ['code' => 'KG', 'name' => 'Quilograma', 'factor' => 1],
+            ['code' => 'H', 'name' => 'Hora', 'factor' => 1],
+            ['code' => 'DIA', 'name' => 'Dia', 'factor' => 1],
+            ['code' => 'CX', 'name' => 'Caixa', 'factor' => 1],
+            ['code' => 'LT', 'name' => 'Litro', 'factor' => 1],
+        ];
+
+        foreach ($rows as $row) {
+            Unit::updateOrCreate(
+                ['code' => $row['code']],
+                [
+                    'name' => $row['name'],
+                    'factor' => $row['factor'],
+                    'is_active' => true,
+                ]
+            );
+        }
     }
 }
