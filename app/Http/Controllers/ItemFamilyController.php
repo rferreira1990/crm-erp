5b68be7411812e6ece78cd45dc5f41c4ef4c73a1
Ricardo <rfpav.ric@gmail.com>
2026-03-19 19:14:16 +0000

    19 de mar. de 2026, 19:14

--- app/Http/Controllers/ItemFamilyController.php
+++ app/Http/Controllers/ItemFamilyController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreItemFamilyRequest;
+use App\Http\Requests\UpdateItemFamilyRequest;
+use App\Models\ItemFamily;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class ItemFamilyController extends Controller
 {
-    //
+    public function index(): View
+    {
+        $itemFamilies = ItemFamily::query()
+            ->orderBy('name')
+            ->paginate(15);
+
+        return view('item-families.index', compact('itemFamilies'));
+    }
+
+    public function create(): View
+    {
+        return view('item-families.create');
+    }
+
+    public function store(StoreItemFamilyRequest $request): RedirectResponse
+    {
+        ItemFamily::create($request->validated());
+
+        return redirect()
+            ->route('item-families.index')
+            ->with('success', 'Família criada com sucesso.');
+    }
+
+    public function edit(ItemFamily $item_family): View
+    {
+        return view('item-families.edit', compact('item_family'));
+    }
+
+    public function update(UpdateItemFamilyRequest $request, ItemFamily $item_family): RedirectResponse
+    {
+        $item_family->update($request->validated());
+
+        return redirect()
+            ->route('item-families.index')
+            ->with('success', 'Família atualizada com sucesso.');
+    }
 }
