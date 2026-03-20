5b8a9375b60231de38d6e6898dd01abc28f0a421
Ricardo <rfpav.ric@gmail.com>
2026-03-20 17:30:51 +0000

    20 de mar. de 2026, 17:30

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreBrandRequest;
+use App\Http\Requests\UpdateBrandRequest;
+use App\Models\Brand;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class BrandController extends Controller
 {
-    //
+    public function index(): View
+    {
+        $brands = Brand::query()
+            ->orderBy('name')
+            ->paginate(15);
+
+        return view('brands.index', compact('brands'));
+    }
+
+    public function create(): View
+    {
+        return view('brands.create');
+    }
+
+    public function store(StoreBrandRequest $request): RedirectResponse
+    {
+        Brand::create($request->validated());
+
+        return redirect()
+            ->route('brands.index')
+            ->with('success', 'Marca criada com sucesso.');
+    }
+
+    public function edit(Brand $brand): View
+    {
+        return view('brands.edit', compact('brand'));
+    }
+
+    public function update(UpdateBrandRequest $request, Brand $brand): RedirectResponse
+    {
+        $brand->update($request->validated());
+
+        return redirect()
+            ->route('brands.index')
+            ->with('success', 'Marca atualizada com sucesso.');
+    }
 }
