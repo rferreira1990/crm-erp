0f769a73ef84bf0ebe4d786f5363c08dede5f167
rferreira1990 <rfpav.ric@gmail.com>
2026-03-25 19:08:17 +0000

    25 de mar. de 2026, 19:08

--- resources/views/company-profile/edit.blade.php
+++ resources/views/company-profile/edit.blade.php
@@ -3,34 +3,30 @@
 @section('title', 'Editar Dados da Empresa')
 
 @section('content')
-    <section class="content-with-menu p-0">
-        <div class="inner-body">
-            <section role="main" class="content-body">
-                <header class="page-header">
-                    <h2>Editar Dados da Empresa</h2>
-                </header>
+    <section role="main" class="content-body">
+        <header class="page-header">
+            <h2>Editar Dados da Empresa</h2>
+        </header>
 
-                <form
-                    action="{{ route('company-profile.update') }}"
-                    method="POST"
-                    enctype="multipart/form-data"
-                >
-                    @csrf
-                    @method('PUT')
+        <form
+            action="{{ route('company-profile.update') }}"
+            method="POST"
+            enctype="multipart/form-data"
+        >
+            @csrf
+            @method('PUT')
 
-                    @include('company-profile.partials.form')
+            @include('company-profile.partials.form')
 
-                    <div class="mt-3">
-                        <button type="submit" class="btn btn-primary">
-                            Gravar
-                        </button>
+            <div class="mt-3">
+                <button type="submit" class="btn btn-primary">
+                    Gravar
+                </button>
 
-                        <a href="{{ route('company-profile.show') }}" class="btn btn-default">
-                            Cancelar
-                        </a>
-                    </div>
-                </form>
-            </section>
-        </div>
+                <a href="{{ route('company-profile.show') }}" class="btn btn-default">
+                    Cancelar
+                </a>
+            </div>
+        </form>
     </section>
 @endsection
