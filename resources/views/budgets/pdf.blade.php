45c9d285b0d6e896336077199874cfaaa14343ff
rferreira1990 <rfpav.ric@gmail.com>
2026-03-25 22:08:45 +0000

    25 de mar. de 2026, 22:08

--- resources/views/budgets/pdf.blade.php
+++ resources/views/budgets/pdf.blade.php
@@ -503,6 +503,7 @@
                         </td>
                         <td>{{ number_format((float) $line->quantity, 0, ',', '.') }}</td>
                         <td>{{ $line->unit_name ?: 'un' }}</td>
+                        <td class="text-right">{{ number_format((float) $line->unit_price, 2, ',', '.') }}€</td>
                         <td>{{ number_format((float) $line->unit_price, 2, ',', '.') }}€</td>
                         <td>{{ number_format((float) $line->discount_percent, 0, ',', '.') }}</td>
                         <td>
