b08b99fe39a59500dcb93e28d3ffefaa290d16d8
rferreira1990 <rfpav.ric@gmail.com>
2026-03-25 18:55:10 +0000

    25 de mar. de 2026, 18:55

---
+++ app/Models/CompanyProfile.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CompanyProfile extends Model
+{
+    protected $fillable = [
+        'owner_id',
+        'company_name',
+        'address_line_1',
+        'address_line_2',
+        'city',
+        'district',
+        'postal_code',
+        'postal_code_suffix',
+        'postal_designation',
+        'country_code',
+        'tax_number',
+        'phone',
+        'fax',
+        'contact_person',
+        'email',
+        'website',
+        'share_capital',
+        'registry_office',
+        'logo_path',
+        'bank_name',
+        'bank_iban',
+        'bank_bic_swift',
+    ];
+
+    protected $casts = [
+        'share_capital' => 'decimal:2',
+    ];
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function getFullPostalCodeAttribute(): ?string
+    {
+        if (!$this->postal_code) {
+            return null;
+        }
+
+        if (!$this->postal_code_suffix) {
+            return $this->postal_code;
+        }
+
+        return $this->postal_code . '-' . $this->postal_code_suffix;
+    }
+}
