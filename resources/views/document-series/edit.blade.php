6745efdb4a09f0be113af41120346e0622ba9041
rferreira1990 <rfpav.ric@gmail.com>
2026-03-26 23:10:02 +0000

    26 de mar. de 2026, 23:10

---
+++ resources/views/document-series/edit.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.admin')
+
+@section('title', 'Editar Série')
+
+@section('content')
+
+<h2 class="mb-3">Editar Série</h2>
+
+<form method="POST" action="{{ route('document-series.update', $documentSeries) }}">
+    @csrf
+    @method('PUT')
+
+    <div class="card">
+        <div class="card-body row g-3">
+
+            <div class="col-md-4">
+                <label>Prefixo</label>
+                <input name="prefix" class="form-control" value="{{ $documentSeries->prefix }}">
+            </div>
+
+            <div class="col-md-4">
+                <label>Série</label>
+                <input name="name" class="form-control" value="{{ $documentSeries->name }}">
+            </div>
+
+            <div class="col-md-4">
+                <label>Ano</label>
+                <input name="year" type="number" class="form-control" value="{{ $documentSeries->year }}">
+            </div>
+
+            <div class="col-md-4">
+                <label>Ativa</label><br>
+                <input type="checkbox" name="is_active" value="1" {{ $documentSeries->is_active ? 'checked' : '' }}>
+            </div>
+
+        </div>
+
+        <div class="card-footer text-end">
+            <button class="btn btn-primary">Guardar</button>
+        </div>
+    </div>
+
+</form>
+
+@endsection
