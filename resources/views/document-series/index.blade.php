f5cdb290f52829a7016180855f680ee3a90e83e2
rferreira1990 <rfpav.ric@gmail.com>
2026-03-26 23:12:45 +0000

    26 de mar. de 2026, 23:12

--- resources/views/document-series/index.blade.php
+++ resources/views/document-series/index.blade.php
@@ -42,7 +42,7 @@
                             @endif
                         </td>
                         <td>
-                            <a href="{{ route('document-series.edit', $s) }}" class="btn btn-sm btn-outline-primary">
+                            <a href="{{ route('document-series.edit', $s) }}" class="mb-1 mt-1 me-1 btn btn-primary">
                                 Editar
                             </a>
                         </td>
