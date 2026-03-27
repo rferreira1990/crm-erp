1f09ecb02436df3185f3c625b7e57164f3595585
rferreira1990 <rfpav.ric@gmail.com>
2026-03-27 23:49:36 +0000

    27 de mar. de 2026, 23:49

--- app/Models/ActivityLog.php
+++ app/Models/ActivityLog.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class ActivityLog extends Model
 {
-    //
+    protected $fillable = [
+        'owner_id',
+        'user_id',
+        'action',
+        'entity',
+        'entity_id',
+        'payload',
+    ];
+
+    protected $casts = [
+        'payload' => 'array',
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationships
+    |--------------------------------------------------------------------------
+    */
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
