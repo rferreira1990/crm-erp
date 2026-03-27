487bd43a5ba4300c1473ebac8223db98590804c6
rferreira1990 <rfpav.ric@gmail.com>
2026-03-27 20:37:34 +0000

    27 de mar. de 2026, 20:37

--- resources/views/budgets/show.blade.php
+++ resources/views/budgets/show.blade.php
@@ -102,6 +102,12 @@
         </div>
     </div>
 
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+
     @if (session('error'))
         <div class="alert alert-danger">
             {{ session('error') }}
