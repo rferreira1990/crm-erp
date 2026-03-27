880ba125b6f6ce1b4ece0ae5db99a1afc14f6ac1
rferreira1990 <rfpav.ric@gmail.com>
2026-03-27 22:39:19 +0000

    27 de mar. de 2026, 22:39

---
+++ app/Policies/AuthServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Budget;
+use App\Models\Item;
+use App\Models\PaymentTerm;
+use App\Policies\BudgetPolicy;
+use App\Policies\ItemPolicy;
+use App\Policies\PaymentTermPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        Budget::class => BudgetPolicy::class,
+        Item::class => ItemPolicy::class,
+        PaymentTerm::class => PaymentTermPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+    }
+}
