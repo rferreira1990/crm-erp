82a31741cc134bfdfc7fa9af9fdcc41af601bd31
rferreira1990 <rfpav.ric@gmail.com>
2026-03-27 22:50:40 +0000

    27 de mar. de 2026, 22:50

--- app/Policies/AuthServiceProvider.php
+++ app/Policies/AuthServiceProvider.php
@@ -3,9 +3,11 @@
 namespace App\Providers;
 
 use App\Models\Budget;
+use App\Models\DocumentSeries;
 use App\Models\Item;
 use App\Models\PaymentTerm;
 use App\Policies\BudgetPolicy;
+use App\Policies\DocumentSeriesPolicy;
 use App\Policies\ItemPolicy;
 use App\Policies\PaymentTermPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -21,6 +23,7 @@ class AuthServiceProvider extends ServiceProvider
         Budget::class => BudgetPolicy::class,
         Item::class => ItemPolicy::class,
         PaymentTerm::class => PaymentTermPolicy::class,
+        DocumentSeries::class => DocumentSeriesPolicy::class,
     ];
 
     /**
