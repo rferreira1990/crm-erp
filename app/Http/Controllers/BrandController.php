788ae6ba9a396b8a00cecfdafca893aee7c43ac0
rferreira1990 <rfpav.ric@gmail.com>
2026-03-27 23:00:10 +0000

    27 de mar. de 2026, 23:00

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -10,40 +10,47 @@ use Illuminate\View\View;
 
 class BrandController extends Controller
 {
-    public function index(): View
-    {
-        $brands = Brand::query()
-            ->orderBy('name')
-            ->paginate(15);
+   public function index(): View
+{
+    $this->authorize('viewAny', Brand::class);
+
+    $brands = Brand::query()
+        ->where('owner_id', auth()->id())
+        ->orderBy('name')
+        ->paginate(15);
 
         return view('brands.index', compact('brands'));
     }
 
     public function create(): View
     {
+        $this->authorize('create', Brand::class);
+
         return view('brands.create');
     }
 
     public function store(StoreBrandRequest $request): RedirectResponse
     {
+        $this->authorize('create', Brand::class);
+
         Brand::create($request->validated());
 
-        return redirect()
-            ->route('brands.index')
-            ->with('success', 'Marca criada com sucesso.');
+        return redirect()->route('brands.index')->with('success', 'Marca criada com sucesso.');
     }
 
     public function edit(Brand $brand): View
     {
+        $this->authorize('update', $brand);
+
         return view('brands.edit', compact('brand'));
     }
 
     public function update(UpdateBrandRequest $request, Brand $brand): RedirectResponse
     {
+        $this->authorize('update', $brand);
+
         $brand->update($request->validated());
 
-        return redirect()
-            ->route('brands.index')
-            ->with('success', 'Marca atualizada com sucesso.');
+        return redirect()->route('brands.index')->with('success', 'Marca atualizada com sucesso.');
     }
 }
