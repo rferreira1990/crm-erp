fd8ae13b0df138d067adc722d7bc67caaf86590d
rferreira1990 <rfpav.ric@gmail.com>
2026-03-28 15:59:04 +0000

    28 de mar. de 2026, 15:59

--- database/migrations/2026_03_28_155820_add_technical_responsible_id_to_works_table.php
+++ database/migrations/2026_03_28_155820_add_technical_responsible_id_to_works_table.php
@@ -6,23 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('works', function (Blueprint $table) {
-            //
+            $table->foreignId('technical_responsible_id')
+                ->nullable()
+                ->after('responsible_user_id')
+                ->constrained('users')
+                ->nullOnDelete();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('works', function (Blueprint $table) {
-            //
+            $table->dropConstrainedForeignId('technical_responsible_id');
         });
     }
 };
