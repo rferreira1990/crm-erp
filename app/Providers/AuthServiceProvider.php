f2eb536c556dedf9ab676ffbaaecd3e2375207ce
rferreira1990 <rfpav.ric@gmail.com>
2026-03-28 16:40:41 +0000

    28 de mar. de 2026, 16:40

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,49 +2,49 @@
 
 namespace App\Providers;
 
+use App\Models\Brand;
 use App\Models\Budget;
 use App\Models\DocumentSeries;
 use App\Models\Item;
-use App\Models\PaymentTerm;
-use App\Models\Brand;
 use App\Models\ItemFamily;
-use App\Models\Unit;
-use App\Models\TaxRate;
+use App\Models\PaymentTerm;
 use App\Models\TaxExemptionReason;
-
+use App\Models\TaxRate;
+use App\Models\Unit;
+use App\Models\User;
+use App\Models\Work;
 use App\Policies\BrandPolicy;
-use App\Policies\ItemFamilyPolicy;
-use App\Policies\UnitPolicy;
-use App\Policies\TaxRatePolicy;
-use App\Policies\TaxExemptionReasonPolicy;
 use App\Policies\BudgetPolicy;
 use App\Policies\DocumentSeriesPolicy;
+use App\Policies\ItemFamilyPolicy;
 use App\Policies\ItemPolicy;
 use App\Policies\PaymentTermPolicy;
+use App\Policies\TaxExemptionReasonPolicy;
+use App\Policies\TaxRatePolicy;
+use App\Policies\UnitPolicy;
+use App\Policies\UserPolicy;
+use App\Policies\WorkPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The policy mappings for the application.
-     *
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        Budget::class => BudgetPolicy::class,
-        Item::class => ItemPolicy::class,
-        PaymentTerm::class => PaymentTermPolicy::class,
-        DocumentSeries::class => DocumentSeriesPolicy::class,
         Brand::class => BrandPolicy::class,
+        Budget::class => BudgetPolicy::class,
+        DocumentSeries::class => DocumentSeriesPolicy::class,
+        Item::class => ItemPolicy::class,
         ItemFamily::class => ItemFamilyPolicy::class,
-        Unit::class => UnitPolicy::class,
-        TaxRate::class => TaxRatePolicy::class,
+        PaymentTerm::class => PaymentTermPolicy::class,
         TaxExemptionReason::class => TaxExemptionReasonPolicy::class,
+        TaxRate::class => TaxRatePolicy::class,
+        Unit::class => UnitPolicy::class,
+        User::class => UserPolicy::class,
+        Work::class => WorkPolicy::class,
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
     public function boot(): void
     {
         $this->registerPolicies();
