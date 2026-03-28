f5cc971b1d34a65b4b1cf4fd212fac85ece000e0
rferreira1990 <rfpav.ric@gmail.com>
2026-03-28 15:58:51 +0000

    28 de mar. de 2026, 15:58

--- database/migrations/2026_03_28_155830_create_work_user_table.php
+++ database/migrations/2026_03_28_155830_create_work_user_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('work_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('work_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
+
+            $table->unique(['work_id', 'user_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('work_user');
