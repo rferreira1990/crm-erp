1a89497752db17e75124b37dc1577e8ad193a156
rferreira1990 <rfpav.ric@gmail.com>
2026-03-28 15:23:28 +0000

    28 de mar. de 2026, 15:23

---
+++ app/Actions/Works/ChangeWorkStatusAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Actions\Works;
+
+class ChangeWorkStatusAction
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
