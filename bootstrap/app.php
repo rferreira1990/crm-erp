13d3a8103405e412a79965637bde2302a17676b5
rferreira1990 <rfpav.ric@gmail.com>
2026-04-06 23:20:11 +0000

    7 de abr. de 2026, 00:20

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request as HttpRequest;
 use App\Http\Middleware\SecurityHeaders;
 use Spatie\Permission\Middleware\PermissionMiddleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
@@ -15,6 +16,38 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $trustedHosts = array_values(array_filter(array_map(
+            static fn (string $host): string => trim($host),
+            explode(',', (string) env('TRUSTED_HOSTS', ''))
+        )));
+
+        $trustedProxyHeaders = match (strtoupper((string) env('TRUSTED_PROXY_HEADERS', ''))) {
+            'HEADER_X_FORWARDED_AWS_ELB' => HttpRequest::HEADER_X_FORWARDED_AWS_ELB,
+            'HEADER_FORWARDED' => HttpRequest::HEADER_FORWARDED,
+            'HEADER_X_FORWARDED_FOR' => HttpRequest::HEADER_X_FORWARDED_FOR,
+            'HEADER_X_FORWARDED_HOST' => HttpRequest::HEADER_X_FORWARDED_HOST,
+            'HEADER_X_FORWARDED_PORT' => HttpRequest::HEADER_X_FORWARDED_PORT,
+            'HEADER_X_FORWARDED_PROTO' => HttpRequest::HEADER_X_FORWARDED_PROTO,
+            'HEADER_X_FORWARDED_PREFIX' => HttpRequest::HEADER_X_FORWARDED_PREFIX,
+            default => HttpRequest::HEADER_X_FORWARDED_FOR
+                | HttpRequest::HEADER_X_FORWARDED_HOST
+                | HttpRequest::HEADER_X_FORWARDED_PORT
+                | HttpRequest::HEADER_X_FORWARDED_PROTO
+                | HttpRequest::HEADER_X_FORWARDED_PREFIX
+                | HttpRequest::HEADER_X_FORWARDED_AWS_ELB,
+        };
+
+        $middleware->trustHosts(
+            at: static fn (): array => $trustedHosts,
+            subdomains: true,
+        );
+
+        $trustedProxies = trim((string) env('TRUSTED_PROXIES', ''));
+        $middleware->trustProxies(
+            at: $trustedProxies !== '' ? $trustedProxies : null,
+            headers: $trustedProxyHeaders,
+        );
+
         $middleware->web(append: [
             SecurityHeaders::class,
         ]);
